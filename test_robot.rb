fba351ab3769128078166d814d3b3415da41434b
Tony <ANCLIST@GMAIL.COM>
2017-10-27 13:14:59 -0400

    Test 1 to 7

--- test_robot.rb
+++ test_robot.rb
@@ -4,76 +4,102 @@ require './robot.rb'
 
 class TestRobot < MiniTest::Test
 
+  def setup
+    @robot = Robot.new
+  end
+
+  def teardown
+    @robot = nil
+  end
+
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
+    # skip
     # arrange
-
+    @robot.needs_repairs = true
+    @robot.foreign_model = true
     # act
-
+    result = @robot.station
+    expect = 1
     # assert
+    assert_equal(expect, result)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
+    # skip
     # arrange
-
+    @robot.needs_repairs = true
+    @robot.vintage_model = true
     # act
-
+    result = @robot.station
+    expect = 2
     # assert
+    assert_equal(expect, result)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
+    # skip
     # arrange
-
+    @robot.needs_repairs = true
     # act
-
+    result = @robot.station
+    expect = 3
     # assert
+    assert_equal(expect, result)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
+    # skip
     # arrange
-
+    @robot.needs_repairs = false
     # act
-
+    result = @robot.station
+    expect = 4
     # assert
+    assert_equal(expect, result)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
+    # skip
     # arrange
-
+    @robot.todos = []
     # act
-
+    result = @robot.prioritize_tasks
+    expect = -1
     # assert
+    assert_equal(expect, result)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
+    # skip
     # arrange
-
+    @robot.todos = [1, 3, 5, 2]
     # act
-
+    result = @robot.prioritize_tasks
+    expect = 5
     # assert
+    assert_equal(expect, result)
   end
 
   def test_workday_on_day_off_returns_false
-    skip
+    # skip
     # arrange
-
+    day_off = "Sunday"
     # act
-
+    result = @robot.workday?("Sunday")
+    expect = false
     # assert
+    assert_equal(expect, result)
   end
 
   def test_workday_not_day_off_returns_true
-    skip
+    # skip
     # arrange
-
+    day_off = "Sunday"
     # act
-
+    result = @robot.workday?("Monday")
+    expect = true
     # assert
+    assert_equal(expect, result)
   end
 
 end
